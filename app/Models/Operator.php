97af1fd1474a7e82354acb87ba33947eb1af96f9
Restu Akbar <rebartheblues1905@gmail.com>
2024-09-18 15:59:26 +0700

    coba

---
+++ app/Models/Operator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Operator extends Authenticatable
+{
+    use HasFactory;
+    protected $guard = 'operator';
+    protected $fillable = [
+        'id',
+        'username',
+        'password',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
