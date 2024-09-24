7b20e7368c7e36fb972a9bfd32a8b7e48114b3df
Melly Dwiliani <mellydwiliani7@gmail.com>
2024-09-24 21:40:15 +0700

    Tampilan Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex align-items-center justify-content-center bg-light" style="height: 100vh;">
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4">
+            <div class="text-center mb-4">
+                <img src="images/PKMPOLBAN.png" alt="PKM POLBAN" class="img-fluid" style="max-width: 350px;">
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <form id="login-form">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Username" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password" required>
+                        </div>
+                        <div class="form-group">
+                            <label id="math-question"></label>
+                            <input type="number" id="math-answer" class="form-control" placeholder="Hasil Penjumlahan" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </form>
+
+                    <a href="#" class="d-block mt-3 text-center text-primary">Permintaan akun terakreditasi internasional</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script>
+    let correctAnswer;
+
+    function generateMathQuestion() {
+        const num1 = Math.floor(Math.random() * 10) + 1;
+        const num2 = Math.floor(Math.random() * 10) + 1;
+        document.getElementById('math-question').innerText = `${num1} + ${num2} = `;
+        correctAnswer = num1 + num2; 
+    }
+
+    document.getElementById('login-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const answer = parseInt(document.getElementById('math-answer').value, 10);
+        if (answer === correctAnswer) {
+            alert("Login berhasil!");
+        } else {
+            alert("Jawaban penjumlahan salah!");
+        }
+    });
+
+    generateMathQuestion();
+</script>
+
+</body>
+</html>
